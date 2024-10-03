3bc53be490d1a2c6543ebcc4d09249e9d0ba42ff
Lupanova Anna <hanna.lupanava@newland.by>
2024-10-03 12:18:20 +0300

    task4

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, HTTPException
-import asyncio
 import aiohttp
 import asyncio
-import uvicorn
+from typing import Optional
+from pytubefix import Stream
+from concurrent.futures import ThreadPoolExecutor
 import os
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 
 app = FastAPI()
 
@@ -24,10 +27,33 @@ async def get_video_data(video_id: str) -> dict:
             return await response.json()
 
 
+def get_stream(link: str) -> Optional[Stream] | None:
+    try:
+        return YouTube(link, on_progress_callback=on_progress).streams.filter(subtype="mp4").order_by("resolution").desc().first()
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+async def fetch_video_info(link: str):
+    loop = asyncio.get_event_loop()
+    with ThreadPoolExecutor() as pool:
+        return await loop.run_in_executor(pool,get_stream, link)
+
+
 @app.get("/get-video-data/{video_id}")
 async def get_data_from_youtube(video_id: str):
     res = await get_video_data(video_id)
     if 'items' not in res or not res['items']:
         raise HTTPException(status_code=404, detail="Failed to fetch video data")
     return res['items'][0]
+
+
+@app.get("/get-download-link/{video_id}")
+async def get_metadata(video_id: str):
+    link = f"https://www.youtube.com/watch?v={video_id}"
+    res = await fetch_video_info(link)
+    if not res:
+        return HTTPException(status_code=404, detail="Failed to fetch video data")
+    return res.url
+
 
