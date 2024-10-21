ef7f32fdc1410922507dca00783d15be1177017d
Anna Lupanova <annalupanova1999@gmail.com>
2024-10-21 20:25:13 +0300

    fix worker

--- worker.py
+++ worker.py
@@ -20,7 +20,10 @@ DEAD_LETTER_QUEUE = "dead_letter_queue"
 MESSAGE_TTL = 1000
 DEAD_LETTER_EXCHANGE = "dlx"
 MAX_RETRIES = 3
-
+dead_letter_args = {
+            "x-message-ttl": MESSAGE_TTL,
+            "x-dead-letter-exchange": DEAD_LETTER_EXCHANGE
+        }
 
 async def send_email(to_email, subject, body):#
     try:
@@ -49,56 +52,24 @@ async def on_message(message: IncomingMessage):
             await message.ack()
         except Exception as e:
             print(f"Error processing message: {e}")
-            await handle_retry(message, body)
-
-async def handle_retry(message: IncomingMessage, body: dict):
-    attempts = body.get("attempts", 1)
-
-    if attempts < 0:
-        attempts += 1
-        body["attempts"] = attempts
-        new_message = Message(body=json.dumps(body).encode())
-
-        try:
             connection = await connect_robust(RABBITMQ_URL)
             async with connection:
                 channel = await connection.channel()
                 await channel.default_exchange.publish(
-                    new_message,
-                    routing_key=QUEUE_NAME
+                    message, routing_key="email_ttl_queue"
                 )
-                logger.info(f"Message requeued for attempt {attempts}")
-        except Exception as e:
-            logger.error(f"Failed to requeue message: {e}")
-
-    else:
-        logger.info("Max retries reached, sending to dead letter queue")
-        try:
-            connection = await connect_robust(RABBITMQ_URL)
-            async with connection:
-                channel = await connection.channel()
-                await channel.default_exchange.publish(
-                    Message(body=message.body),
-                    routing_key=DEAD_LETTER_QUEUE
-                )
-                await message.ack()
                 logger.info("Message sent to dead letter queue")
-        except Exception as e:
-            logger.error(f"Failed to send to dead letter queue: {e}")
+
 
 async def main():
     connection = await connect_robust(RABBITMQ_URL)
     async with connection:
         channel = await connection.channel()
         await channel.set_qos(prefetch_count=1)
-        dead_letter_args = {
-            "x-message-ttl": MESSAGE_TTL,
-            "x-dead-letter-exchange": DEAD_LETTER_EXCHANGE
-        }
-        queue = await channel.declare_queue(QUEUE_NAME, arguments=dead_letter_args)
-        await queue.consume(on_message)
-        dead_letter_queue = await channel.declare_queue(DEAD_LETTER_QUEUE, durable=True)
 
+        queue = await channel.declare_queue(QUEUE_NAME)
+        await queue.consume(on_message)
+        dead_letter_queue = await channel.declare_queue(DEAD_LETTER_QUEUE, durable=True, arguments=dead_letter_args)
         await asyncio.Future()
 
 if __name__ == '__main__':
